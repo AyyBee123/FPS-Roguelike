633664caee683c5fb4efa4f3ef228ba382668c55
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-10-23 10:49:06 +0400

    removed outlines

--- Scripts/Pickups/item.gd
+++ Scripts/Pickups/item.gd
@@ -1,4 +1,14 @@
 extends Node3D
 
+var time: float = 0.0
+var amplitude: float = 0.1
+var frequency: float = 2.0
+
+@onready var mesh_instance = %MeshInstance
+@onready var default_pos = mesh_instance.get_position()
+
 func _physics_process(delta):
 	rotate_y(PI/2 * delta)
+	
+	time += delta * frequency
+	mesh_instance.set_position(default_pos + Vector3(0, sin(time) * amplitude, 0))
