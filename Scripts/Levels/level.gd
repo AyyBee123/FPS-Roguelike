6352dacbb248141ff1d64aada173fcc56dd726c2
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-11-07 17:35:01 +0400

    added countdown timer

--- Scripts/Levels/level.gd
+++ Scripts/Levels/level.gd
@@ -1,1 +1,11 @@
 extends Node3D
+
+var time_left: float = 1200.0 # time in seconds (20 minutes, in this case)
+
+func _physics_process(delta):
+	time_left = max(time_left - delta, 0)
+	if time_left <= 0: # do stuff when countdown time reaches zero
+		pass
+
+func get_time_left():
+	return time_left
--- Scripts/Utils/enemy_manager.gd
+++ Scripts/Utils/enemy_manager.gd
@@ -11,8 +11,13 @@ var MIN_AMOUNT_OF_ENEMIES: int = 10
 var SPAWNING_INTERVAL: float = 3
 var t: float = 0
 
+# distance range the enemy can spawn away from the player
 const MIN_DISTANCE: float = 25
 const MAX_DISTANCE: float = 50
+
+# distance range that groups of enemies can spawn from each other
+const MIN_GROUP_DISTANCE: float = 6
+const MAX_GROUP_DISTANCE: float = 12
 
 const ENEMY = preload("uid://bf7ljiiykmoi0")
 
@@ -41,17 +46,23 @@ func _physics_process(delta):
 				spawn_enemy(enemies.pick_random()) # for now
 
 func spawn_enemy(spawn: EnemySpawn) -> void:
+	var random_position = get_random_position()
 	for i in range(spawn.amount_to_spawn):
 		var enemy = spawn.enemy.instantiate()
 		current_number_of_enemies += 1
-		enemy.position = get_random_position()
+		enemy.position = random_position + get_random_group_offset()
 		get_tree().current_scene.add_child.call_deferred(enemy)
 
 func get_random_position() -> Vector3:
 	var pos: Vector3 = player.global_position
 	var spawn_point: Vector2 = Vector2(pos.x, pos.z) + \
 			(Vector2.ONE * randf_range(MIN_DISTANCE, MAX_DISTANCE)).rotated(randf_range(0, TAU))
-	return Vector3(spawn_point.x, 30, spawn_point.y)
+	return Vector3(spawn_point.x, 1000, spawn_point.y)
+
+func get_random_group_offset() -> Vector3:
+	var offset: Vector2 = (Vector2.ONE * randf_range(MIN_GROUP_DISTANCE, MAX_GROUP_DISTANCE)) \
+			.rotated(randf_range(0, TAU))
+	return Vector3(offset.x, 0, offset.y)
 
 func _on_enemy_defeat(_enemy) -> void:
 	current_number_of_enemies -= 1
