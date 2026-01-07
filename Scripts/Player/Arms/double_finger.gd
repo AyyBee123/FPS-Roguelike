d586f63933c42ba06dfdb6bb2a85809d1cd00abb
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2026-01-07 21:00:49 +0400

    remodeled double finger

--- Scripts/Player/Arms/double_finger.gd
+++ Scripts/Player/Arms/double_finger.gd
@@ -1,13 +1,27 @@
 extends Arm
 
-@onready var bullet_point_2 = %"Bullet Point 2"
+@onready var muzzle_point = %"Muzzle Point"
+@onready var muzzle_point_2 = %"Muzzle Point2"
 
 func shoot():
-	super.shoot()
-	
 	if t < fire_rate_timer: return
 	
+	t = 0.0
+	fire_rate_timer = 1.0 / fire_rate
+	animation_player.stop()
+	animation_player.play(shoot_animation)
 	if muzzle:
 		var m = muzzle.instantiate()
 		m.set_color(muzzle_color)
-		bullet_point_2.add_child(m)
+		muzzle_point.add_child(m)
+		
+		var m2 = muzzle.instantiate()
+		m2.set_color(muzzle_color)
+		muzzle_point_2.add_child(m2)
+	
+	var camera_collision = get_camera_collision()
+	
+	player._on_arm_shot(self)
+	
+	for i in range(projectile_count):
+		launch_projectile(camera_collision)
