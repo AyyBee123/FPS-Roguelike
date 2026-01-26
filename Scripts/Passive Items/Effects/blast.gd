ff1710522653dd43fb41148e1005040f09679075
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2026-01-26 23:07:05 +0400

    particle optimization

--- Scripts/Passive Items/Effects/blast.gd
+++ Scripts/Passive Items/Effects/blast.gd
@@ -24,7 +24,7 @@ func _ready():
 		if particle.material_override is StandardMaterial3D:
 			particle.material_override.albedo_color = color
 		elif particle.material_override is ShaderMaterial:
-			particle.material_override.set_shader_parameter("albedo", color)
+			particle.set_instance_shader_parameter("albedo_color", color)
 		max_lifetime = max(particle.lifetime, max_lifetime)
 	
 	await get_tree().create_timer(max_lifetime).timeout
