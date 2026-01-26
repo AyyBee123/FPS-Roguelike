3aaee70c5e2d4da519395ac223d3c6d09d1e1d4d
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2026-01-27 00:22:52 +0400

    Update chest_manager.gd

--- Scripts/Utils/chest_manager.gd
+++ Scripts/Utils/chest_manager.gd
@@ -10,12 +10,14 @@ func spawn_chests():
 		var chest = CHEST.instantiate()
 		chest.position = level.find_chest_spawn()
 		level.add_child(chest)
+		await get_tree().physics_frame
 
 func spawn_armory_boxes():
 	for i in range(level.NUMBER_OF_ARMORY_BOXES):
 		var armory_box = ARMORY_BOX.instantiate()
 		armory_box.position = level.find_chest_spawn()
 		level.add_child(armory_box)
+		await get_tree().physics_frame
 
 func _on_timer_timeout():
 	spawn_chests()
