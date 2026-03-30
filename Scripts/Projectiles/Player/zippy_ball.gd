dfe3f13b9b8fb79bbaa88639bb0345e5c07f0151
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2026-03-30 07:42:46 +0400

    added max amount of zippy balls

--- Scripts/Projectiles/Player/zippy_ball.gd
+++ Scripts/Projectiles/Player/zippy_ball.gd
@@ -4,14 +4,23 @@ extends "res://Scripts/Projectiles/Player/projectile.gd"
 
 @onready var ball = %Ball
 
+const MAX_AMOUNT: int = 30
+
+static var balls: Array[RigidBody3D]
 var material: StandardMaterial3D
 var color: Color
-var bounces: int = 5
+var bounces: int = 4
 
 func _ready():
 	super._ready()
 	ball.set_instance_shader_parameter("zippy_albedo", color)
 	bounces = floori(range / 100)
+
+func _physics_process(delta):
+	super._physics_process(delta)
+	if balls.size() > MAX_AMOUNT:
+		if self == balls[0]:
+			queue_free()
 
 func _on_body_entered(body):
 	bounces -= 1
@@ -30,3 +39,6 @@ func explode():
 	blast.color = color
 	blast.position = position
 	get_tree().current_scene.add_child(blast)
+
+func _exit_tree():
+	balls.erase(self)
