0aa6930878f1b5838c34690d7149308b95b5bdab
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2026-04-08 20:13:38 +0400

    added a cooldown to lightning strike and increased its damage

--- Scripts/Abilities/Effects/lightning_strike.gd
+++ Scripts/Abilities/Effects/lightning_strike.gd
@@ -2,10 +2,13 @@ extends Ability
 
 @export var LIGHTNING: PackedScene
 
-const DAMAGE_MULTIPLIER: float = 0.5
+@onready var cooldown = %Cooldown
+
+const DAMAGE_MULTIPLIER: float = 1
 
 func _on_hit(enemy: Enemy, source: Variant, damage: float):
 	if not enemy or source.is_in_group("Lightning Strike"): return
+	if not cooldown.is_stopped(): return
 	var lightning = LIGHTNING.instantiate()
 	lightning.visible = false
 	lightning.position = enemy.position
@@ -14,3 +17,4 @@ func _on_hit(enemy: Enemy, source: Variant, damage: float):
 	lightning.player = player
 	get_tree().current_scene.add_child(lightning)
 	lightning.visible = true
+	cooldown.start()
