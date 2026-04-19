32da83ee73899a948205b5ec7d802d59d9fce606
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2026-04-20 03:28:27 +0400

    Update ability_upgrade_resource.gd

--- Scripts/Resources/ability_upgrade_resource.gd
+++ Scripts/Resources/ability_upgrade_resource.gd
@@ -1,7 +1,8 @@
 class_name AbilityUpgradeResource extends Resource
 
 @export_enum("Common", "Uncommon", "Legendary", "Rare") var rarity: int # rarity the stat shows up
-@export_enum("Damage", "Fire_Rate", "Range", "Speed", "Splash_Radius", "Projectile_Count", "Extra_Jumps") var stat: String
+@export_enum("Damage", "Fire_Rate", "Range", "Speed", "Splash_Radius", 
+		"Projectile_Count", "Extra_Jumps", "Jump_Height") var stat: String
 @export var amount: float
 @export_enum("+", "x", "flat") var type: String
 @export var upgrade_for_player: bool = false # checks whether an upgrade is for the player or ability
